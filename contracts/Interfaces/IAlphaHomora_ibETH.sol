abbdbd22095eced266f4d54df0fcd092fe6f85e0
jack0x-tech <jack@stacker.vc>
2021-01-27 18:25:00 -0800

    change bridge to ibETH, dump yEarn

--- contracts/Interfaces/IAlphaHomora_ibETH.sol
+++ contracts/Interfaces/IAlphaHomora_ibETH.sol
@@ -4,12 +4,9 @@ pragma solidity ^0.6.11;
 
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol"; // call ERC20 safely
 
-interface IVault {
+interface IAlphaHomora_ibETH {
 	using SafeERC20 for IERC20;
 	
-	function deposit(uint256 _amount) external;
-	function depositETH() payable external;
+	function deposit() payable external;
 	function withdraw(uint256 _shares) external;
-	function withdrawETH(uint256 _shares) external;
-	function token() external view returns (IERC20);
 }
---
+++ contracts/Token/Bridge_AlphaHomora_ibETH.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: MIT
+/*
+A bridge that connects yEarn Vault contracts to our STACK gauge contracts. 
+This allows users to submit only one transaction to go from (supported ERC20 <-> yEarn vault <-> STACK commit to VC fund)
+They will be able to deposit & withdraw in both directions.
+*/
+
+pragma solidity ^0.6.11;
+
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol"; // call ERC20 safely
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+import "../Interfaces/IAlphaHomora_ibETH.sol";
+import "../Interfaces/IGaugeD1.sol";
+
+contract VaultGaugeBridge is ReentrancyGuard {
+	using SafeERC20 for IERC20;
+	using Address for address;
+    using SafeMath for uint256;
+
+    address payable public constant AlphaHomora_ibETH = 0x67B66C99D3Eb37Fa76Aa3Ed1ff33E8e39F0b9c7A;
+
+    address public governance;
+    address public gauge;
+
+    constructor () public {
+    	governance = msg.sender;
+    }
+
+    receive() external payable {
+        if (msg.sender != AlphaHomora_ibETH){
+            // if the fund is open, then hard commit to the fund, if it's not, then fallback to soft commit
+            if (IGaugeD1(gauge).fundOpen()){
+                depositBridgeETH(true); 
+            }
+            else {
+                depositBridgeETH(false);
+            } 
+        }
+    }
+
+    function setGovernance(address _new) external {
+        require(msg.sender == governance, "BRIDGE: !governance");
+        governance = _new;
+    }
+
+    // create a new bridge, warning this allows an overwrite
+    function setGauge(address _gauge) external {
+    	require(msg.sender == governance, "BRIDGE: !governance");
+        require(gauge == address(0), "BRIDGE: gauge already set");
+
+    	gauge = _gauge;
+    }
+
+    // deposit ETH into ETH vault. WETH can be done with normal depositBridge call.
+    // public because of fallback function
+    function depositBridgeETH(bool _commit) nonReentrant public payable {
+    	require(gauge != address(0), "BRIDGE: !bridge"); // need to setup, fail
+
+    	uint256 _beforeToken = IERC20(AlphaHomora_ibETH).balanceOf(address(this));
+    	IAlphaHomora_ibETH(AlphaHomora_ibETH).deposit{value: msg.value}();
+    	uint256 _afterToken = IERC20(AlphaHomora_ibETH).balanceOf(address(this));
+    	uint256 _receivedToken = _afterToken.sub(_beforeToken);
+
+    	_depositGauge(_receivedToken, _commit, msg.sender);
+    }
+
+    // withdraw as ETH from WETH vault. WETH withdraw can be from from depositBridge call.
+    function withdrawBridgeETH(uint256 _amount) nonReentrant external {
+        require(gauge != address(0), "BRIDGE: !bridge"); // need to setup, fail
+
+        uint256 _receivedToken = _withdrawGauge(_amount, msg.sender);
+
+        uint256 _before = address(this).balance;
+        IAlphaHomora_ibETH(AlphaHomora_ibETH).withdraw(_receivedToken);
+        uint256 _after = address(this).balance;
+        uint256 _received = _after.sub(_before);
+
+        msg.sender.transfer(_received);
+    }
+
+    function _withdrawGauge(uint256 _amount, address _user) internal returns (uint256){
+        uint256 _beforeToken = IERC20(AlphaHomora_ibETH).balanceOf(address(this));
+        IGaugeD1(gauge).withdraw(_amount, _user);
+        uint256 _afterToken = IERC20(AlphaHomora_ibETH).balanceOf(address(this));
+
+        return _afterToken.sub(_beforeToken);
+    }
+
+    function _depositGauge(uint256 _amount, bool _commit, address _user) internal {
+		IERC20(AlphaHomora_ibETH).safeApprove(gauge, 0);
+    	IERC20(AlphaHomora_ibETH).safeApprove(gauge, _amount);
+
+    	if (_commit){
+    		IGaugeD1(gauge).deposit(0, _amount, _user);
+    	}
+    	else {
+    		IGaugeD1(gauge).deposit(_amount, 0, _user);
+    	}
+    }
+}
